46767f7f53f485d72b77bebbd040346148721022
rvincoy <vin23002@byui.edu>
2026-05-08 22:14:05 -0500

    update message and routes.rest for prod

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -62,7 +62,7 @@ const deleteContact = async (req, res, next) => {
   const result = await mongodb.getDb().db().collection('contacts').deleteOne({ _id: userId });
   if (result.deletedCount > 0) {
     res.setHeader('Content-Type', 'application/json');
-    res.status(204).json({ message: 'Contact deleted successfully' });
+    res.status(200).json({ message: 'Contact deleted successfully' });
   } else {
     res.status(404).json({ message: 'Contact not found' });
   }
